f90d83fa4e0f467ee179274cd043a653a79b1f3c
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-11-19 08:49:42 +0200

    copy from codesandbox my index.js

--- week2/src/index.js
+++ week2/src/index.js
@@ -12,8 +12,10 @@ let days = [
 let hoursStr = date.getHours() + "";
 let hours = hoursStr.length > 1 ? hoursStr : `0${hoursStr}`;
 let minutesStr = "" + date.getMinutes();
+
 let minutes = minutesStr.length > 1 ? minutesStr : `0${minutesStr}`;
 dateTime.innerHTML = `${days[date.getDay()]} ${hours}:${minutes}`;
+
 // maybe i'll change this
 function changeMinutes() {
   setInterval(function () {
@@ -25,12 +27,21 @@ function changeMinutes() {
 }
 changeMinutes();
 // Search-form
+let h1 = document.querySelector("h1");
+let units = "metric";
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let h1 = document.querySelector("h1");
   let searchInput = document.querySelector("#search-input");
   h1.innerHTML = searchInput.value;
+  let city = h1.innerHTML;
+  let apikey = "88724523008dc9e1be18f6eb6a959b67";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
+  axios.get(url).then((response) => {
+    let temperature = Math.round(response.data.main.temp);
+    curDegrees.innerHTML = temperature;
+    console.log(response.data);
+  });
 });
 //Celsius to fahrenheit
 let linkCelsius = document.querySelector("#celsius");
@@ -51,6 +62,7 @@ linkCelsius.addEventListener("click", (event) => {
     linkCelsius.classList.add("active");
     linkFahrenheit.classList.remove("active");
   }
+  units = "metric";
 });
 linkFahrenheit.addEventListener("click", (event) => {
   event.preventDefault();
@@ -59,4 +71,15 @@ linkFahrenheit.addEventListener("click", (event) => {
     linkCelsius.classList.remove("active");
     linkFahrenheit.classList.add("active");
   }
+  units = "imperial";
 });
+// Search engine
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  curDegrees.innerHTML = temperature;
+}
+let apikey = "88724523008dc9e1be18f6eb6a959b67";
+let city = h1.innerHTML;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
+
+axios.get(url).then(showTemperature);
