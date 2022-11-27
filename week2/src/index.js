9e108df98bf355d824ba2b236e33455cccfb8e7a
Blackdoll-Loki <helpmeloki7@gmail.com>
2022-11-27 09:26:08 +0200

    add humidity and wind to forecast

--- week2/src/index.js
+++ week2/src/index.js
@@ -16,6 +16,8 @@ let minutesStr = "" + date.getMinutes();
 let minutes = minutesStr.length > 1 ? minutesStr : `0${minutesStr}`;
 dateTime.innerHTML = `${days[date.getDay()]} ${hours}:${minutes}`;
 
+let searchBtn = document.querySelector("#search-btn");
+
 // maybe i'll change this
 function changeMinutes() {
   setInterval(function () {
@@ -26,23 +28,11 @@ function changeMinutes() {
   }, 60000);
 }
 changeMinutes();
-// Search-form
+
 let h1 = document.querySelector("h1");
 let units = "metric";
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  h1.innerHTML = searchInput.value;
-  let city = h1.innerHTML;
-  let apikey = "88724523008dc9e1be18f6eb6a959b67";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
-  axios.get(url).then((response) => {
-    let temperature = Math.round(response.data.main.temp);
-    curDegrees.innerHTML = temperature;
-    console.log(response.data);
-  });
-});
+
 //Celsius to fahrenheit
 let linkCelsius = document.querySelector("#celsius");
 let linkFahrenheit = document.querySelector("#fahrenheit");
@@ -75,11 +65,33 @@ linkFahrenheit.addEventListener("click", (event) => {
 });
 // Search engine
 function showTemperature(response) {
+  let humidityPar = document.querySelector("#humidity");
+  let windPar = document.querySelector("#wind");
   let temperature = Math.round(response.data.main.temp);
   curDegrees.innerHTML = temperature;
+  humidityPar.innerHTML = response.data.main.humidity;
+  windPar.innerHTML = response.data.wind.speed;
 }
 let apikey = "88724523008dc9e1be18f6eb6a959b67";
 let city = h1.innerHTML;
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
 
 axios.get(url).then(showTemperature);
+function getCityWeather() {
+  let searchInput = document.querySelector("#search-input");
+  if (searchInput.value !== "") {
+    h1.innerHTML = searchInput.value;
+  } else {
+    return;
+  }
+  let city = h1.innerHTML;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`;
+  axios.get(url).then((response) => {
+    showTemperature(response);
+  });
+}
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getCityWeather();
+});
+searchBtn.addEventListener("click", () => getCityWeather());
